190f16aa4a2e9c3ab24c75127d92b361a47fcbc0
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-08-04 12:00:00 +0900

    Chore: Add Presentation Folder

---
+++ Projects/MatchLab/Resources/Presentation/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MatchLab
+//
+//  Created by 나현흠 on 8/6/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
