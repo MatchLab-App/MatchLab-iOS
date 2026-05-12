e43b333ea664132644bebcb6c3c2e9345e64eca8
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-05-12 17:30:32 +0900

    [fix] add accessibility Label in Nav Bar

--- Projects/MatchLab/Resources/Common/Components/NavigationBar.swift
+++ Projects/MatchLab/Resources/Common/Components/NavigationBar.swift
@@ -21,6 +21,7 @@ struct NavigationBar: View {
                     .padding(.vertical, 13)
                     .padding(.horizontal, 12)
             })
+            .accessibilityLabel(isSelected ? String(localized: "accessibility.back") : String(localized: "accessibility.menu"))
             
             Spacer()
             
@@ -28,6 +29,7 @@ struct NavigationBar: View {
                 .resizable()
                 .scaledToFit()
                 .padding(.vertical, 14)
+                .accessibilityLabel(String(localized: "accessibility.logo"))
             
             Spacer()
             
@@ -41,6 +43,7 @@ struct NavigationBar: View {
                     .padding(.horizontal, 12)
                     .opacity(isSelected ? 1 : 0)
             })
+            .accessibilityHidden(!isSelected)
         }
         .padding(.horizontal, 30)
         .frame(height: 47)
